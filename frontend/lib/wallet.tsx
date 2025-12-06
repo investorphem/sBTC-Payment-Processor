6342901e12cfa3270f850a8dcf1263cf37849349
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-06 18:32:30 +0100

    Add files via upload

--- frontend/lib/wallet.tsx
+++ frontend/lib/wallet.tsx
@@ -1,1 +1,36 @@
+import React from 'react';
+import { showConnect, openContractCall } from '@stacks/connect';
+import { AppConfig } from '@stacks/connect';
 
+const appConfig = new AppConfig(['store_write', 'publish_data']);
+
+export function connectWallet() {
+  showConnect({
+    appDetails: { name: 'sBTC Payment Processor', icon: '/favicon.ico' },
+    onFinish: payload => {
+      // payload contains auth response
+      window.location.reload();
+    },
+    appConfig,
+  });
+}
+
+export async function callCreateInvoice({
+  contractAddress,
+  contractName,
+  functionName,
+  functionArgs,
+  network,
+  onFinish,
+  postConditionMode,
+}: any) {
+  return openContractCall({
+    contractAddress,
+    contractName,
+    functionName,
+    functionArgs,
+    appDetails: { name: 'sBTC Payment Processor', icon: '/favicon.ico' },
+    network,
+    onFinish,
+  });
+}
