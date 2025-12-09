f9061492d71aec283d36236299243a1f90783799
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-09 15:41:05 +0100

    Update payment_test.ts

--- contracts/tests/payment_test.ts
+++ contracts/tests/payment_test.ts
@@ -5,7 +5,7 @@ Clarinet.test('invoice create & pay flow (simulated)', async (chain: Chain, acco
   const customer = accounts.get('wallet_2')!;
 
   // merchant creates invoice
-  const block = chain.mineBlock([
+  const block = chain.minelock([
     Tx.conrctCall('sbtc-paymentprocessor', 'create-ivoice', [Tx.cv_u128(1000), Tx.cv_buff(Buffer.from('sBTC')), Tx.cv_some(Tx.cv_rincipal(Buffer.from('SP000000000000000000002Q6VF78.sbtc-token'))), Tx.cv_none()], merchant.address),
   ]);
 
