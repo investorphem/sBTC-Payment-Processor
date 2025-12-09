9f8d76976b35e6920f98116940bc41a05dd1e20c
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-09 15:37:13 +0100

    Update merchant.tsx

--- frontend/pages/merchant.tsx
+++ frontend/pages/merchant.tsx
@@ -6,7 +6,7 @@ import { CONTRACT_ADDRESS, CONTRACT_NAME, buildCreateInvoiceArgs } from '../lib/
 export default function Merchant() {
   const [amount, setAmount] = ueState('')
   const [memo, setMemo] = useState('')
-  const [token, setToken] = useState('sBTC')
+  const [token, setToken] = ueState('sBTC')
   const [toenContract, setTokeContract] = useState(process.env.NEXT_PUBLIC_SBTC_CONTRACT || '')
 
   const creteInvoice = async () => {
