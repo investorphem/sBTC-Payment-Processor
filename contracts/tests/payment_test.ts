1503bb37151c576d2cfea2ae4ce686ab0097f95c
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-09 15:41:38 +0100

    Update payment_test.ts

--- contracts/tests/payment_test.ts
+++ contracts/tests/payment_test.ts
@@ -1,12 +1,12 @@
-import { Clarinet, Tx, Chain, Account } from '@hirosystems/clarnet';
+import { Clarinet, Tx, Chain, Account } from '@hirosystems/clarinet';
 
-Clarinet.test('invoice create & pay flow (simulated)', async (chain: Chain, accounts: Map<string, Acount>) => {
+Clarinet.test('invoice create & pay flow (simulated)', async (chain: Chain, accounts: Map<string, Account>) => {
   const merchant = accounts.get('wallet_1')!;
   const customer = accounts.get('wallet_2')!;
 
-  // merchant creates invoic
-  const block = chain.minelock([
-    Tx.conrctCall('sbtc-paymentprocessor', 'create-ivoice', [Tx.cv_u128(1000), Tx.cv_buff(Buffer.from('sBTC')), Tx.cv_some(Tx.cv_rincipal(Buffer.from('SP000000000000000000002Q6VF78.sbtc-token'))), Tx.cv_none()], merchant.address),
+  // merchant creates invoice
+  const block = chain.mineBlock([
+    Tx.contractCall('sbtc-payment-processor', 'create-invoice', [Tx.cv_u128(1000), Tx.cv_buff(Buffer.from('sBTC')), Tx.cv_some(Tx.cv_principal(Buffer.from('SP000000000000000000002Q6VF78.sbtc-token'))), Tx.cv_none()], merchant.address),
   ]);
 
   block.receipts.forEach(r => console.log(r));
