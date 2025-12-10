686fe7b9da7f389fe3475e9e991ac4aabace675a
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-10 21:59:24 +0100

    Update webhook.ts

--- frontend/pages/api/webhook.ts
+++ frontend/pages/api/webhook.ts
@@ -1,4 +1,4 @@
-// Optional: example webhook endpoint to receive notifications from an off-chai
+// Optional: example webhook endpoint to receive notifications from an off-cha
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
