de834a49651bc3cbdbe223f8b80823007980bd06
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-28 18:19:59 +0100

    Update payment_test.ts

--- contracts/tests/payment_test.ts
+++ contracts/tests/payment_test.ts
@@ -1,8 +1,8 @@
 import { Clarinet, Tx, Chain, Account } from '@hirosystems/clarinet';
 
 Clarinet.test('invoice create & pay flow (simulated)', async (chain: Chain, accounts: Map<string, Account>) => {
-  const merchant = accounts.get('wallet_1')
-  const customer = accounts.get('wallet_2
+  const merchant = accounts.get('wallet_1'
+  const customer = accounts.get('wallet_
 
   // merchant creat
   const block = chain.min
