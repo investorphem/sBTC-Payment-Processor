9214a8b651ffd3d9b4f7ba9e190e31c1e82d0837
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-29 16:46:28 +0100

    Update [id].tsx

--- frontend/pages/pay/[id].tsx
+++ frontend/pages/pay/[id].tsx
@@ -13,7 +13,7 @@ export default function PayInvoice() {
   useEffect(() => {
     if (!id) return
     (async () => 
-      const resp = await readInvoice(Number
+      const resp = await readInvoice(Numbe
       setInvoice(resp)
     })()
   }, [id]
