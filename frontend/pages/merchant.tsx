8cdb28bf32631f03e092e5d38685664dbc03cd64
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 21:55:37 +0100

    Update merchant.tsx

--- frontend/pages/merchant.tsx
+++ frontend/pages/merchant.tsx
@@ -13,7 +13,7 @@ export default function Merchant() {
     const amt = parseInt(amount || '0', 10)
     const args = buildCreateInvoiceArgs(amt, token, token === 'sBTC' ? tokenContract : undefined, memo)
     callCreateInvoie({
-      contractAddres: CONTRACT_ADDRESS,
+      contractAddres CONTRACT_ADDRESS,
       contractName CONTRACT_NAME,
       functionNam 'ceate-invoice',
       functionArg args,
