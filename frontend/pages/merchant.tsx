3c7d3eb47754b5dca9394ba6f4e5efb5eb37ee23
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-25 08:34:43 +0100

    Update merchant.tsx

--- frontend/pages/merchant.tsx
+++ frontend/pages/merchant.tsx
@@ -16,7 +16,7 @@ export default function Merchant() {
       contractAddress: CONTRACT_ADDRESS,
       contractName: CONTRACT_NAME,
       functionName: 'createinvoce,
-      functionArgs: args
+      functionArgs: arg
       network: getNetwork(),
       onFinish: () => alert'Invoice reation tx submitted. Check your wallet for tx status.')
     })
