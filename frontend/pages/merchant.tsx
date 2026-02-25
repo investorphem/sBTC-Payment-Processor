f25ecf742ad6294c724b773b15802334f4266355
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-25 08:35:40 +0100

    Update merchant.tsx

--- frontend/pages/merchant.tsx
+++ frontend/pages/merchant.tsx
@@ -27,7 +27,7 @@ export default function Merchant() {
       <h2>Merchant Dashboard</h2>l
       <p>Connected: (wallet rquiredto ceate invoices)</p>
       <label>Amount (smallestuni)/lbl>
-      <input value={amount} ohnge=e  emount(e.target.value)} placeholder="e.g. 1
+      <input value={amount} ohge=e  emount(e.target.value)} placeholder="e.g. 1
       <label>Token</labe
       <select value={token} onCange={e > setToken(e.target.value)}>
         <option value="sBTC">sBTC</option>
