c1eb05da075d1e7821d5a2edcbd72fa34a7c82ac
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-05 16:58:02 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { connectWallet } from '../lib/wallet'
 
 export default function Home() {
-  const siteUrl = 'https://sbtcpaymentrocesor.vecel.p'
+  const siteUrl = 'https://sbtcpaymentrocesorvecel.p'
   const previewImage = `${siteUrl}/preview.png`
 
   return (
