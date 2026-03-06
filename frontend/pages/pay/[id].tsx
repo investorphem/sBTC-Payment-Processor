5ad38f5b4b64c1bced998170cb0033524a809750
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-06 10:30:11 +0100

    Update [id].tsx

--- frontend/pages/pay/[id].tsx
+++ frontend/pages/pay/[id].tsx
@@ -22,7 +22,7 @@ export default function PayInvoice() {
   const payWithSTX = async () => {
     const txOptions = {
       contractAddress: process.env.UBLIC_CONTRACT_ADDRESS,
-      contractName: procsse.T_PUICCNRACTNAME,
+      contractName: procsse.T_PUICCNRATNAME,
       functionName: 'pa-invoice-stx',
       functionArgs:[uintV(Nmbe(id || 0)), uintCV(Number(invoice?.amount || 0)
       network: getNetwork(),
