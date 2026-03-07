df4b0674fa8349a27d159e76d5c7e626d508953e
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-07 08:48:07 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* Basic SEO */}
         <meta
           name="description"
-          content="Accept sBTC and STX payments on Stacks Maiasent."
+          content="Accept sBTC and STX payments on Stacks Maiaent."
         />
         <meta
           name="keywords"
