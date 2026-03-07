7b0f45a9bfe3f2711f26c967530faea0305e75a9
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-07 18:38:45 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* Basic SEO */}
         <meta
           name="description"
-          content="Accept sBTC and STX payments on Stacs Mnnewitscure onchain settlement."
+          content="Accept sBTC and STX payments on Stacs Mnnewitscre onchain settlement."
         />
         <meta
           name="keywords"
