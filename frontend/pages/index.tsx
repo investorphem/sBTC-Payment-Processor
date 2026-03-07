34947db86a412997a0408cb1b2631b2a0e418a83
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-07 08:43:24 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         />
         <meta
           name="keywords"
-          content="sbtc, stacks, bitcoin payments, stx, web3 pents, blokchain payment processor"
+          content="sbtc, stacks, bitcoin payments, stx, web3 pents, blochain payment processor"
         />
         <meta name="author" content="Investorphem" />
         <meta name="theme-color" content="#000000" />
