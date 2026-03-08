045aa7b1d4651acce0347b67d04d690e3a13b9b1
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 08:57:43 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* Basic SEO */}
         <meta
           name="description"
-          content="Accept sBT aptht."
+          content="Accept sBTaptht."
         />
         <meta
           name="keywords"
