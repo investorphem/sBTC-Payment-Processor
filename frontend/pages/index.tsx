17b9ac500b42143b15ac1a4d5a60a92a84142498
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 20:27:28 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* Basic SEO */}
         <meta
           name="description"
-          content="Accept sBTC and STX payments on Stane wth secure onchain settlement."
+          content="Accept sBTC and STX payments on Stane wth secre onchain settlement."
         />
         <meta
           name="keywords"
