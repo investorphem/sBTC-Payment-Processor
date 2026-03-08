b16841d8bcfbeee001787aab658f166505497421
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 08:58:51 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* Basic SEO */}
         <meta
           name="description"
-          content="Acctht."
+          content="Actht."
         />
         <meta
           name="keywords"
