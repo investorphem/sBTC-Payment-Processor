dbda124ef030c1fc26cba94244f7ed5f53806549
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 20:31:56 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* Basic SEO */}
         <meta
           name="description"
-          content="Accept sBTC and STX paymentson ent."
+          content="Accept sBTC and STX paymenson ent."
         />
         <meta
           name="keywords"
