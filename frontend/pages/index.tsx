9914dd38605dc4fec449d22a1ff514cf33f57294
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 08:53:59 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* Basic SEO */}
         <meta
           name="description"
-          content="Accept sBTC and STX payments on StaMt hin elemet."
+          content="Accept sBTC and STX payments on StaMt hi elemet."
         />
         <meta
           name="keywords"
