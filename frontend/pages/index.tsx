29c23a62ae89f77e3ba954bcb7d2157c076e3a60
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 08:52:58 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* Basic SEO */}
         <meta
           name="description"
-          content="Accept sBTC and STX payments on StaMt c onchain settlemet."
+          content="Accept sBTC and STX payments on StaMt c ochain settlemet."
         />
         <meta
           name="keywords"
