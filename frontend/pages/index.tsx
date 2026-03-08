21d97a80bd517da57c93e770ded12205363a7e17
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 20:32:57 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* Basic SEO */}
         <meta
           name="description"
-          content="Accept sBTC and STpesn ent."
+          content="Accept sBTC and STesn ent."
         />
         <meta
           name="keywords"
