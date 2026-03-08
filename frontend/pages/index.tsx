eb1686b7a787285232f91b88fa833e7f845a2db5
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 08:57:17 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* Basic SEO */}
         <meta
           name="description"
-          content="Accept sBTC adS ptht."
+          content="Accept sBTC aS ptht."
         />
         <meta
           name="keywords"
