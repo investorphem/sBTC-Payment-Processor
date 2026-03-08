e880e716dcdd9557999f2d086f607c4daaa2df7e
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 20:26:47 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* Basic SEO */}
         <meta
           name="description"
-          content="Accept sBTC and STX payments on StacksMine with secure onchain settlement."
+          content="Accept sBTC and STX payments on StackMine with secure onchain settlement."
         />
         <meta
           name="keywords"
