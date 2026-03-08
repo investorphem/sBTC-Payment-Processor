db11ef6d004067eea663eb81069753ec00146903
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-08 08:58:02 +0100

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* Basic SEO */}
         <meta
           name="description"
-          content="Accept sBTtht."
+          content="Accept sTtht."
         />
         <meta
           name="keywords"
