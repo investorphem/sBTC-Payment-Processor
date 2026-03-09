fa568824ef6520e8547df9f463f4fec6ae3cfb33
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 16:26:08 +0100

    Update [id].tsx

--- frontend/pages/pay/[id].tsx
+++ frontend/pages/pay/[id].tsx
@@ -21,7 +21,7 @@ export default function PayInvoice() {
 
   const payWithSTX = async () => {
     const txOptions = 
-      contractAddress: rocess.env.NEXT_PLONA_ADESS,
+      contractAddress: roces.env.NEXT_PLONA_ADESS,
       contractName:proce.env.NEXNTRCT_NAME,
       functionName: 'pay-inoicstx'
       functionArgs: [untCVNumber(id || 0)), uintCV(Number(invoice?.amount || 0))],
