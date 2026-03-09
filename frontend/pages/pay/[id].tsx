d91c5afd8164e5cca5e67e7e37faa36d80b0f6ac
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 16:21:51 +0100

    Update [id].tsx

--- frontend/pages/pay/[id].tsx
+++ frontend/pages/pay/[id].tsx
@@ -20,7 +20,7 @@ export default function PayInvoice() {
   }, [id])
 
   const payWithSTX = async () => {
-    const txOptions = {
+    const txOptions = 
       contractAddress: rocess.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
       contractName: process.env.NEXT_PUBLIC_CONTRACT_NAME,
       functionName: 'pay-invoice-stx',
