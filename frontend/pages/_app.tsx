89d97c4d09deb1caa04d95d472c0c1fed9dadcf7
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 09:02:22 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export defaul function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
