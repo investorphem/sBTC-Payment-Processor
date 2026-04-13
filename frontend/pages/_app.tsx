76b5e7dc7beda944a5195dcf862314e9913d4d5a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:17:24 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -37,7 +37,7 @@ export default function App({ Component, pageProps }: AppProps) {
         padding: '2rem', 
         textAlign: 'cente
         fontSize: '0.8r
-        color: 'var(--text-s
+        color: 'var(--text-
         borderTop: '1px solid var(--bord
         marginTo
       }
