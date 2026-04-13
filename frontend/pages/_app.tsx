9892614f7cc75620692e97d86528db2f563d5b9c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:20:08 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -39,7 +39,7 @@ export default function App({ Component, pageProps }: AppProps) {
         fontSize: '0.
         color: 'var(
         borderTop: '1px solid var(--bord
-        marginT
+        margin
       
         Built on Stacks & sB
       </footer>
