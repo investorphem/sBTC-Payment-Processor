d4a8220dc9a2cdc60bde90054020195107a50818
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:14:05 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -38,7 +38,7 @@ export default function App({ Component, pageProps }: AppProps) {
         textAlign: 'center'
         fontSize: '0.8rem
         color: 'var(--text-second
-        borderTop: '1px solid var(--border-col
+        borderTop: '1px solid var(--border-co
         marginTop: '40
       }}>
         Built on Stacks & sBT
