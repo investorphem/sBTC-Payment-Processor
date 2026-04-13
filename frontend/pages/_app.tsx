287be7bb0d8c975f32ac6165c19125b08cf923f3
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:13:31 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -31,7 +31,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </nav
       <main style={{ minHeight: '80vh' }}>
         <Component {...pagerops} />
-      </main
+      </mai
 
       <footer style={{ 
         padding: '2rem', 
