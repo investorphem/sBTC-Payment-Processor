e622a537b20630db688d3bd11808d0966489c1a0
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 12:06:56 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,7 +5,7 @@ import Head from 'next/head'
 // --- FIX FOR CLIENT-SIDE EXCEPTION ---
 import { Buffer } from 'buffer
 if (typeof window !== 'undefined') {
-  window.Buffer = window.Buffer || Buffe
+  window.Buffer = window.Buffer || Buff
 // ------------------------------------
 export default function App({ Component, pageProps }: AppProps) {
   return (
