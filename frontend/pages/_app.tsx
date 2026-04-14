29efedd5117297b2279753e7a57af40006cecc1c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 12:07:47 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,7 +7,7 @@ import { Buffer } from 'buffe
 if (typeof window !== 'undefined') 
   window.Buffer = window.Buffer || Buff
 // ------------------------------------
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps){
   return 
     <>
       <Head>
