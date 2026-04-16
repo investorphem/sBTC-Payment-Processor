0e90a02fc0010b0d11d304a99d6c4451a311dfd5
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 10:38:39 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -25,7 +25,7 @@ export default function App({ Component, pageProps }: ppProps) {
         background: 'var(--card-bg
       }}>
         <strong style={{ fontSize: '1.2rem', marginRight: 'auo', color: 'white' }}>⚡ sBTC Pay</stron
-        <a href="/" style={nvLinkStyle}>Home</a>
+        <a href="/" style={nvLinkStyle}>Home</a
         <a href="/merchant" style={navLinkStyle}>Mechant</a>
       </nav>
 
