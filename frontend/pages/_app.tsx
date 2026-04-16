06aa9fa5634026b5f410c2b58ef1e3086f7609e9
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 10:41:08 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,7 +7,7 @@ import { Buffer } from 'buffer'
 if (typeof window !== 'undefined') {
   window.Buffer = window.Buffer || Buffer
 
-// -------------------------------------
+// ------------------------------------
 export default function App({ Component, pageProps }: ppProps) {
   return 
     <>
