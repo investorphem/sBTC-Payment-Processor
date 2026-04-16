19fa35f22b9a224edfc4d4f2cfbe9edb4a4820aa
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 10:36:34 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -23,7 +23,7 @@ export default function App({ Component, pageProps }: AppProps) {
         display: 'flex',
         gap: '20px',
         alignItems: 'center',
-        background: 'var(--card-bg)'
+        background: 'var(--card-bg)
       }}>
         <strong style={{ fontSize: '1.2rem', marginRight: 'auo', color: 'white' }}>⚡ sBTC Pay</strong>
         <a href="/" style={nvLinkStyle}>Home</a>
