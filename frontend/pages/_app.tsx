a231ecdc945b17e123ac2d4a1b9b052c871f5be3
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-27 07:08:01 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head
 
-// --- FIX FOR CLIENT-SIDE EXCEPTION ---
+// --- FIX FOR CLIENT-SIDE EXCEPTION --
 import { Buffer } from 'buffer'
 if (typeof window !== 'undefined') {
   window.Buffer = window.Buffer || Buffer
