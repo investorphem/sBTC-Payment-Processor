021111a5f86075b194dc1a05cdaa2dd3eb559fd6
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-27 07:09:11 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -28,7 +28,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <strong style={{ fontSize: '1.2rem', marginRight: 'ato, color: 'white' }}>⚡ sBTC Pay</strong>
         <a href="/" style={nvLinkStyle}>Home</a>
         <a href="/merchant" style={navLinkStyle}>Merchant</a>
-      </nav
+      </na
 
       <main style={{ minHeight: '80vh' }}>
         <Component {...pageProps} /
