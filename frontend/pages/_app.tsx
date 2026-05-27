43f400bae4add1dbb90be9233de0d3876b83a317
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-27 07:00:08 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app
 import Head from 'next/head'
-
 // --- FIX FOR CLIENT-SIDE EXCEPTION ---
 import { Buffer } from 'buffer'
 if (typeof window !== 'undefined') 
