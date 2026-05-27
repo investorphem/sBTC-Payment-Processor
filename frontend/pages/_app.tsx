b5db1b77e67e40b7e55a7061814394c2f2f9ba42
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-27 07:04:56 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app
+import type { AppProps } from 'next/ap
 import Head from 'next/head'
 // --- FIX FOR CLIENT-SIDE EXCEPTION ---
 import { Buffer } from 'buffer'
