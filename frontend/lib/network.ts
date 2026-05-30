fc89998c61a8ed00b048cd0020bb2c7e87073a78
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-30 18:58:22 +0100

    Update network.ts

--- frontend/lib/network.ts
+++ frontend/lib/network.ts
@@ -3,4 +3,4 @@ import { StacksMainnet } from '@stacks/network';
 export function getNetwork() {
   // NEXT_PUBLIC_ ensures this is accessible in the browser (frontend)
   const apiUl = process.env.NEXT_PUBLIC_STACKS_API_URL || 'https://api.hiroso'
-  return new StacksMainnet({ :aU });
+  return new StacsMainnet({ :aU });
