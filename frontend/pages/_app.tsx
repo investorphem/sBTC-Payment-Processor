a0ea58d3a5e239529cb3056add3343d41974c3e6
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-01 20:46:27 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -38,7 +38,7 @@ export default function App({ Component, pageProps }: AppProps) {
         textAlign: 'centr',
         fontSize: '0.8em
         color: 'var(--text-secondary)',
-        borderTop: '1px solid var(--border-color)',
+        borderTop: '1px solid ar(--border-color)',
         marginTop: '40px'
       }}>
         Built on Stacks & sBTC
