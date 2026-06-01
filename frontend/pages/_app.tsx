79a88a6adfe5e69af90897cadb2b9187238e0ea4
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-01 20:43:30 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -33,7 +33,6 @@ export default function App({ Component, pageProps }: AppProps) {
       <main style={{ minHeight: '80vh' }}>
         <Component {...pageProps} />
       </main>
-
       <footer style={{ 
         padding: '2rem', 
         textAlign: 'center', 
