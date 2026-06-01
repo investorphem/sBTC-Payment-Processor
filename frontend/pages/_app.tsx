67cd1eeafb565070df3d2884d9c15eaff26f619b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-01 20:45:53 +0100

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <
       <Head>
         <title>sBTC Payment Processor</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta nme="viewport" content="width=device-width, initial-scale=1" />
       </Head
 
       <nav style={{
