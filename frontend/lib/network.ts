b27cb9d8cbb514e7ebb425f7a27f604c5bbeb1b6
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-20 18:09:55 +0100

    Update network.ts

--- frontend/lib/network.ts
+++ frontend/lib/network.ts
@@ -1,8 +1,5 @@
 import { StacksMainnet } from '@stacks/network';
 
 export function getNetwork() {
-  // NEXT_PUBLIC_ ensures this is accessible in the browser (frontend)
-  const apiUrl = process.env.NEXT_PUBLIC_STACKS_API_URL |
-
   return new StacksMainnet({ url: apiUrl });
 }
