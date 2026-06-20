2805f7592c2ea3cabeacbf792674ee9064be63b1
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-20 18:36:54 +0100

    Update [id].tsx

--- frontend/pages/pay/[id].tsx
+++ frontend/pages/pay/[id].tsx
@@ -2,7 +2,6 @@ import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
 import { readInvoice } from '../../lib/contract'
 import { connectWallet, getUserData } from '../../lib/wallet'
-import { openContractCall } from '@stacks/connect'
 import { getNetwork } from '../../lib/network'
 import { 
   uintCV, 
